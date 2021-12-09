3c943d183ef9adf910a009ddf562d19e8e844001
nac62116 <cnash@cnash.de>
2021-12-09 18:30:59 +0100

    Optimized starter by catching error

--- RemEx_Editor/starter.bat
+++ RemEx_Editor/starter.bat
@@ -1,4 +1,17 @@
-call npm install
-call npm audit fix
-call start "" http://127.0.0.1:8080/index.html
-call npm start
+call npm install && (
+  call npm audit fix
+  call start "" http://127.0.0.1:8080/index.html
+  call npm start
+) || (
+  echo:
+  echo ####################################################################################################
+  echo:
+  echo ### Could not run the local http-server. Please check if you have Node.js installed.
+  echo ### If not visit https://nodejs.org/en/download/. Else reinstall it.
+  echo ### Still not working, please contact the support with the information that you had a starter error.
+  echo:
+  echo ####################################################################################################
+  echo:
+  pause
+)
+
